be0ce5cd5ec6537a9e06470ddf2a4a292432ed5f
Lok-kun Tsui <lktsui@unm.edu>
2016-09-16 16:58:35 -0600

    Set labels and grid on load of scan data to same as post-fit

--- CarbonXS_GUI.py
+++ CarbonXS_GUI.py
@@ -229,6 +229,11 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.ax = self.fig.add_subplot(111)
         self.ax.plot(self.x_data, self.y_data)
 
+        self.ax.tick_params(axis='both', which='major', labelsize=14)
+        self.ax.set_xlabel('2 $\\theta$ / Degrees', fontsize=14)
+        self.ax.set_ylabel(r'Intensity / a.u.', fontsize=14)
+        self.ax.legend(fontsize=14, frameon=True)
+        self.ax.grid(True)
         self.canvas.draw()
 
     def plot_fit_results(self):
