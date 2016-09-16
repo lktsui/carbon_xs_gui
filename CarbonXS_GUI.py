e971992ba5c5992d4d6d3e65a48ca235b66d5376
Lok-kun Tsui <lktsui@unm.edu>
2016-09-16 17:02:22 -0600

    Increased line width to 2

--- CarbonXS_GUI.py
+++ CarbonXS_GUI.py
@@ -227,7 +227,7 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
         self.fig.delaxes(self.ax)
         self.ax = self.fig.add_subplot(111)
-        self.ax.plot(self.x_data, self.y_data)
+        self.ax.plot(self.x_data, self.y_data, linewidth = 2, label="Source")
 
         self.ax.tick_params(axis='both', which='major', labelsize=14)
         self.ax.set_xlabel('2 $\\theta$ / Degrees', fontsize=14)
@@ -253,8 +253,8 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
         self.fig.delaxes(self.ax)
         self.ax = self.fig.add_subplot(111)
-        self.ax.plot(self.x_data, self.y_data, label="Source",)
-        self.ax.plot(self.x_fit_data, self.y_fit_data, label="Fit",)
+        self.ax.plot(self.x_data, self.y_data, label="Source", linewidth = 2)
+        self.ax.plot(self.x_fit_data, self.y_fit_data, label="Fit", linewidth = 2)
 
         self.ax.tick_params(axis='both', which='major', labelsize=14)
         self.ax.set_xlabel('2 $\\theta$ / Degrees', fontsize=14)
