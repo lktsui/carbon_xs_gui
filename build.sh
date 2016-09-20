0489dfd0201aab2399d8cee72735dd132b66215c
Lok-kun Tsui <lktsui@unm.edu>
2016-09-20 14:32:24 -0600

    Added a build script for py2exe

---
+++ build.sh
@@ -0,0 +1,14 @@
+rm -r build
+rm -r dist
+python setup.py py2exe
+cd dist
+mkdir carbonxs
+mkdir config
+cd config
+mkdir "diffractometer settings"
+mkdir "fitting parameters"
+mkdir "fitting settings"
+cd ..
+mkdir results
+cp -r icons dist/
+cp ../carbonxs/CARBONXS.exe carbonxs/carbonxs.exe
