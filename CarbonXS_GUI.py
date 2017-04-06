df6dbc9bf57055b3de93f28f6e2b2d5c0f8d6d39
Lok-kun Tsui <lktsui@unm.edu>
2017-04-06 11:17:06 -0600

    Added check that A, d002, La, and M are positive numbers

--- CarbonXS_GUI.py
+++ CarbonXS_GUI.py
@@ -1305,8 +1305,18 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         errors = 0
         warnings = 0
 
-
-
+        if self.param_7.value < 0:
+            print "ERROR: A (In place cell constant) must be greater than 0."
+            errors += 1
+        if self.param_8.value < 0:
+            print "ERROR: d002 (Interlayer Spacing) must be greater than 0."
+            errors += 1
+        if self.param_9.value < 0:
+            print "ERROR: La (Coherence Length) must be greater than 0."
+            errors += 1
+        if self.param_10.value < 0:
+            print "ERROR: M (Number of Layers) must be greater than 0."
+            errors += 1
 
         if self.param_12.value() < 0:
             print "ERROR: Dab (In plane strain) must be greater than 0."
