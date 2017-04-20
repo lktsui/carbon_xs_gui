97cd56d520bdb4cf4af0c2135fadd5931292833d
Lok-kun Tsui <lktsui@unm.edu>
2017-04-20 16:32:55 -0600

    Added build script to automatically generate zip file for windows build

--- build.sh
+++ build.sh
@@ -16,3 +16,6 @@ cp -r README.md dist
 cp LICENSE dist
 cp winlib/*.dll dist/carbonxs
 git archive master --format zip -o dist/src.zip
+rm dist.zip
+7z a dist.zip ./dist/*
+mv dist.zip versions/dist_$(date +%F-%H%M).zip
