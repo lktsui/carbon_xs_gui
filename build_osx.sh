894f8204303703473da7c883d30d54b9e10b530e
Lok-kun Tsui <lktsui@unm.edu>
2017-04-27 17:00:05 -0600

    build script creates a custom timestamp folder and moves the CarbonXS_GUI.app file to it for OSX

--- build_osx.sh
+++ build_osx.sh
@@ -14,3 +14,8 @@ cp carbonxs/carbon.inp dist/CarbonXS_GUI.app/Contents/MacOs/carbonxs
 cp fonts/SourceCodePro-Regular.ttf dist/CarbonXS_GUI.app/Contents/MacOs/fonts
 cp fonts/OFL.txt dist/CarbonXS_GUI.app/Contents/MacOs/fonts
 cp -r icons dist/CarbonXS_GUI.app/Contents/MacOs/
+timestamp=`date +%Y%m%d-%H%M`
+osx_version=`sw_vers -productVersion`
+folder=build_osx${osx_version}_${timestamp}
+mkdir dist/$folder
+mv dist/CarbonXS_GUI.app dist/$folder/CarbonXS_GUI.app
